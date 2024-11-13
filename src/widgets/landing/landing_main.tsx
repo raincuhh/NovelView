fdbb4601fdef6ca8984840a1724431310e44c4ec
raincuhh <146904648+raincuhh@users.noreply.github.com>
2024-11-13 13:41:50 +0100

    images

--- src/widgets/landing/landing_main.tsx
+++ src/widgets/landing/landing_main.tsx
@@ -2,11 +2,22 @@ export default function LaunchMain(): JSX.Element {
    return (
       <>
          <main className="my-16 mx-auto w-[45rem] max-w-[90%] h-full">
-            <div className="flex flex-col select-none h-full">
-               <div
-                  id="bg"
-                  className="w-full h-2/4 bg-slate-200"
-               ></div>
+            <div className="flex flex-col select-none">
+               <div className="relative">
+                  <div
+                     id="bg"
+                     className="absolute w-full h-2/4 bg"
+                  >
+                     {/* <img
+                        src="../../../public/assets/images/launch_bg.png"
+                        alt="launch bg"
+                        className="bg-cover"
+                     /> */}
+                  </div>
+               </div>
+               <div className="flex items-end h-full">
+                  ni
+               </div>
             </div>
          </main>
       </>
