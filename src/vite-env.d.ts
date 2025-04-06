25c10149b9f6b4b953e62a558e45a5c29254fb73
Filip Ryan <146904648+raincuhh@users.noreply.github.com>
2025-04-06 20:04:43 +0200

    removed need for powersync development token

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -15,7 +15,6 @@ interface ImportMetaEnv {
 	readonly VITE_SUPABASE_URL: string;
 	readonly VITE_SUPABASE_ANON_KEY: string;
 	readonly VITE_POWERSYNC_URL: string;
-	readonly VITE_POWERSYNC_TOKEN: string;
 }
 
 interface ImportMeta {
