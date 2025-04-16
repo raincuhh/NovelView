26bc8cc99cdf7eeda4040bbc158526b1712f8c3b
Filip Ryan <146904648+raincuhh@users.noreply.github.com>
2025-04-16 07:08:39 +0200

    Update recents.tsx

--- native/src/pages/home/components/ui/recents.tsx
+++ native/src/pages/home/components/ui/recents.tsx
@@ -2,7 +2,7 @@ export default function Recents() {
 	return (
 		<div className="px-4">
 			<div className="flex flex-col gap-2">
-				<h1 className="text-2xl">Continue</h1>
+				<h1 className="text-2xl font-semibold">Continue</h1>
 			</div>
 		</div>
 	);
