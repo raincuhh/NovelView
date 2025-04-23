8778c3943f8c434b4289ac26e316fb5b7242d0d3
Filip Ryan <146904648+raincuhh@users.noreply.github.com>
2025-04-24 01:55:04 +0200

    Update placeholderLibraryCover.tsx

--- native/src/features/libraries/components/ui/placeholderLibraryCover.tsx
+++ native/src/features/libraries/components/ui/placeholderLibraryCover.tsx
@@ -9,7 +9,7 @@ const PlaceholderLibraryCover = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDi
 			{...props}
 			className={cn("w-full h-full flex justify-center items-center bg-secondary-alt", props.className)}
 		>
-			<Icon.book className="fill-faint w-7 h-7" />
+			<Icon.book className="fill-faint w-[50%] h-[50%]" />
 		</div>
 	);
 });
